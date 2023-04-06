4f9dade18315ae2608cec6547d900033e383cefe
JoshDPT <jjewcu@gmail.com>
2023-04-05 21:15:12 -0400

    add api rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: 'https://modulith.herokuapp.com/',
+        destination: '/',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
