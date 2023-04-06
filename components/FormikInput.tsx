2d8825ffa7e8690847d5e77119d1f198d1657bdb
JoshDPT <jjewcu@gmail.com>
2023-04-05 22:08:59 -0400

    add formik input component

---
+++ components/FormikInput.tsx
@@ -0,0 +1,101 @@
+import { Label } from "flowbite-react";
+import { ErrorMessage, Field, useField } from "formik";
+
+// Validated with FORMIK
+interface FormikProps {
+  id: string;
+  name: string;
+  label: string;
+  placeholder?: string;
+  as?: string;
+  options?: string[] | number[];
+  errors?: Error;
+  touched?: Touched;
+  type?: "text" | "password" | "email" | "date";
+  width?: string;
+}
+
+interface Error {
+  email: string;
+  password: string;
+}
+interface Touched {
+  email: boolean;
+  password: boolean;
+}
+
+export default function FormikInput({
+  id,
+  name,
+  label,
+  placeholder,
+  as,
+  options,
+  type,
+  width,
+}: FormikProps) {
+  width = width ? `w-${width}` : "w-full";
+  const [field, meta] = useField(name);
+
+  if (as) {
+    return (
+      <div className={`${width}`}>
+        <div className="mb-2 block">
+          <Label htmlFor={id} value={label} />
+        </div>
+        <Field
+          id={id}
+          name={name}
+          as={as}
+          className={`${
+            field && meta.error && meta.touched
+              ? "border-red-500 ring-red-500"
+              : ""
+          } 
+          block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500`}
+        >
+          {options?.map((e) => (
+            <option value={e} key={e}>
+              {e}
+            </option>
+          ))}
+        </Field>
+        <div className="h-2">
+          <ErrorMessage
+            name={name}
+            component="div"
+            className="p-1 text-sm font-bold text-red-600"
+          />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={`${width}`}>
+        <div className="mb-2 block">
+          <Label htmlFor={id} value={label} />
+        </div>
+        <Field
+          id={id}
+          name={name}
+          placeholder={placeholder}
+          type={type}
+          className={`${
+            field && meta.error && meta.touched
+              ? "border-red-500 ring-red-500"
+              : ""
+          } 
+
+          block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500`}
+        />
+        <div className="h-2">
+          <ErrorMessage
+            name={name}
+            component="div"
+            className="p-1 text-sm font-bold text-red-600"
+          />
+        </div>
+      </div>
+    );
+  }
+}
