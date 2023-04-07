00b62a3f6b2f69f2844926a4480a42a9f9633b89
JoshDPT <jjewcu@gmail.com>
2023-04-06 23:45:27 -0400

    public login features complete

--- components/UpcomingAvailabilityList.tsx
+++ components/UpcomingAvailabilityList.tsx
@@ -3,6 +3,7 @@
 // GETs availability and lists times by a selected upcoming date (eg: 2023-03-30), starting with tomorrow. You can use a <select> to choose availability, or get creative with it if you so choose.
 
 export default function UpcomingAvailabilityList() {
+  
   return (
     <div>
       Enter
