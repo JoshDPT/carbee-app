e2c8b926ff35ee82306630f458ebe4b5c777e8e3
JoshDPT <jjewcu@gmail.com>
2023-04-07 12:35:00 -0400

    fix UpcomingAvailabilityList functionality

--- components/AppointmentCard.tsx
+++ components/AppointmentCard.tsx
@@ -1,13 +1,53 @@
 import { Card } from 'flowbite-react';
+import { StringLiteral } from 'typescript';
 
-export default function AppointmentCard() {
+// shape of data:
+// {
+//   "id": "string",
+//   "userId": "string",
+//   "duration": 0,
+//   "scheduledTime": "2023-04-07T13:32:00.029Z",
+//   "workOrderDto": {
+//     "service": "string",
+//     "status": "READY",
+//     "notes": "string",
+//     "startTime": "2023-04-07T13:32:00.029Z",
+//     "completeTime": "2023-04-07T13:32:00.029Z",
+//     "paymentTime": "2023-04-07T13:32:00.029Z",
+//     "cancelTime": "2023-04-07T13:32:00.029Z"
+//   },
+//   "status": "SCHEDULED",
+//   "completeTime": "2023-04-07T13:32:00.029Z",
+//   "paymentId": "string",
+//   "cancelTime": "2023-04-07T13:32:00.029Z"
+// }
+
+interface AppointmentProps {
+  duration: number;
+  scheduledTime: string;
+  workOrderDto: {
+    service: string;
+    status: string;
+    notes: string;
+    startTime: string;
+    completeTime: string;
+    paymentTime: string;
+    cancelTime: string;
+  }
+}
+
+export default function AppointmentCard(appointment: AppointmentProps) {
 	return (
 		<div>
 			<Card>
 				<div className="divide-y divide-gray-200">
-					<div>Child element 1</div>
-					<div>Child element 2</div>
-					<div>Child element 3</div>
+					<div>{appointment.scheduledTime}</div>
+					<div>Started/Completed
+						{appointment.workOrderDto.startTime} - {appointment.workOrderDto.completeTime}
+					</div>
+					<div>Service
+						{appointment.workOrderDto.service}
+					</div>
 				</div>
 			</Card>
 		</div>
--- components/UpcomingAvailabilityList.tsx
+++ components/UpcomingAvailabilityList.tsx
@@ -3,58 +3,44 @@ import { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 import getConfig from 'next/config';
 import { useSession } from 'next-auth/react';
+import AppointmentCard from './AppointmentCard';
 
 const { publicRuntimeConfig } = getConfig();
 
 export default function UpcomingAvailabilityList() {
-  // pull the session token from the server
+	// pull the session token from the server
 	const { data: session } = useSession();
 
-	// Define a reducer function to handle the increment/decrement logic
-	function reducer(state: number, action: PageAction) {
-		switch (action.type) {
-			case 'increment':
-				return state + 1;
-			case 'decrement':
-				return state > 0 ? state - 1 : 0;
-			default:
-				throw new Error();
-		}
-	}
-  // initialize state for appointments
+	// initialize state for appointments
 	const [appointments, setAppointments] = useState<object[] | null>(null);
-
-	// Use the useReducer hook to manage the state and dispatch function
-	const [count, dispatch] = useReducer(reducer, 0);
+	const [appointmentNum, setAppointmentNum] = useState(-1);
 
 	useEffect(() => {
-		if (count) {
-			fetchAvailability();
-		}
-	}, [count]);
+		handleAppointmentChange(true);
+	}, []);
 
-	// Define event handlers for the increment and decrement buttons
-	function handleIncrement() {
-		dispatch({ type: 'increment' });
-	}
-	function handleDecrement() {
-		dispatch({ type: 'decrement' });
-	}
-	// Pagination not working: 
-	// fetch protected data and update the appointment options
-	async function fetchAvailability() {
-		const appointmentsData = (await axios.get(
-			`${publicRuntimeConfig.DOMAIN_URL}/server/api/v1/appointments?page=${count}&size=1`,
-			{
-				headers: {
-					Authorization: `Bearer ${session?.user?.accessToken}`,
-				},
-			}
-		)).data;
-    if(appointmentsData.length === 0) {
-      return
-    }
-		console.log(appointmentsData);
+	async function handleAppointmentChange(increment: boolean) {
+
+		const newAppointmentNum = increment
+			? appointmentNum + 1
+			: appointmentNum - 1;
+
+		if (newAppointmentNum < 0) return;
+
+		const appointmentsData = (
+			await axios.get(
+				`${publicRuntimeConfig.DOMAIN_URL}/server/api/v1/appointments?page=${newAppointmentNum}&size=1`,
+				{
+					headers: {
+						Authorization: `Bearer ${session?.user?.accessToken}`,
+					},
+				}
+			)
+		).data;
+		// If there are no appointments at that page #, return without incrementing/decrementing
+		if (!appointmentsData.length) return;
+
+		setAppointmentNum(newAppointmentNum);
 		setAppointments(appointmentsData);
 	}
 
@@ -63,13 +49,19 @@ export default function UpcomingAvailabilityList() {
 		<div className="w-96 mx-auto">
 			<Card className="flex flex-col items-center justify-center p-4">
 				<h1 className="flex items-center justify-center w-36 h-12 text-lg font-bold text-white bg-slate-500 rounded-full mx-auto">
-					Count: {count}
+					Count: {appointmentNum}
 				</h1>
 				<div className="flex justify-center gap-6">
-					<Button className="w-36" onClick={handleDecrement}>
+					<Button
+						className="w-36"
+						onClick={() => handleAppointmentChange(false)}
+					>
 						Previous
 					</Button>
-					<Button className="w-36" onClick={handleIncrement}>
+					<Button
+						className="w-36"
+						onClick={() => handleAppointmentChange(true)}
+					>
 						Next
 					</Button>
 				</div>
@@ -77,12 +69,14 @@ export default function UpcomingAvailabilityList() {
 			{appointments &&
 				appointments.length > 0 &&
 				appointments?.map((appointment) => (
-					<Card
-						key={appointment.id}
-						className="flex flex-col justify-center content-center p-4 w-96 h-12"
-					>
-						<h1>{appointment.id}</h1>
-					</Card>
+					// <Card
+					// 	key={appointment.id}
+					// 	className="flex flex-col justify-center content-center p-4 w-96 h-12"
+					// >
+					// 	<h1>{appointment.id}</h1>
+					// 	<h1>{appointment.workOrderDto.service}</h1>
+					// </Card>
+					<AppointmentCard appointment={appointment}/>
 				))}
 		</div>
 	);
